1f995a462df74976f0242efa1cad8c27312fd054
minechoo <choominhae@gmail.com>
2023-06-28 11:18:23 +0900

    유트브 썸네일 클릭 완성

--- src/components/sub/Youtube.js
+++ src/components/sub/Youtube.js
@@ -6,6 +6,7 @@ import Modal from '../common/Modal';
 function Youtube() {
 	const modal = useRef(null);
 	const [Vids, setVids] = useState([]);
+	const [Index, setIndex] = useState(0);
 
 	useEffect(() => {
 		const key = 'AIzaSyANMdnk7q2cBX8tqGJZXpVFH9bGJMOwmEc'; //api 키
@@ -33,7 +34,14 @@ function Youtube() {
 								</p>
 								<span>{vid.snippet.publishedAt.split('T')[0].split('-').join('.')}</span>
 							</div>
-							<div className='pic' onClick={() => modal.current.open()}>
+							<div
+								className='pic'
+								onClick={() => {
+									modal.current.open();
+									// 썸네일 클릭시 현재 클릭한 요수의 순번값으로 Index state 값 변경
+									setIndex(idx);
+								}}
+							>
 								<img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
 							</div>
 						</article>
@@ -41,7 +49,14 @@ function Youtube() {
 				})}
 			</Layout>
 
-			<Modal ref={modal} />
+			<Modal ref={modal}>
+				{/* 첫 랜더링 싸이클에서는 Vids[0]의 객체값 자체가 없으므로 업는 요소의 id값 호출오류 */}
+				{/* 옵셔널 체이닝은 객체에서만 */}
+				<iframe
+					title={Vids[Index]?.id}
+					src={`https://youtube.com/embed/${Vids[Index]?.snippet.resourceId.videoId}`}
+				></iframe>
+			</Modal>
 		</>
 	);
 }
