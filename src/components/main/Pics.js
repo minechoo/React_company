d68505c66f02f9b1e1d209f17458aceb4864e07c
minechoo <choominhae@gmail.com>
2023-06-29 12:44:36 +0900

    카카오 지도 시작

--- src/components/main/Pics.js
+++ src/components/main/Pics.js
@@ -1,9 +1,19 @@
 function Pics({ Scrolled, Pos }) {
+	const currentPos = Scrolled - Pos;
 	console.log(Scrolled);
-
+	const base = window.innerHeight / 2;
+	const modified = currentPos + base;
 	return (
 		<section id='pics' className='myScroll'>
-			<h1 style={{ transform: `translateX(${Scrolled - Pos}px)` }}>FLICKR</h1>
+			<h1 style={{ transform: `translateX(${currentPos}px)` }}>FLICKR</h1>
+			<article
+				style={{
+					transform: `translate(-50%, -50%) rotate(${Scrolled >= Pos - base ? modified : 0}deg) scale(${
+						Scrolled >= Pos - base ? 1 + modified / 500 : 1
+					}) `,
+					opacity: `${Scrolled >= Pos - base ? 1 - modified / 500 : 1}`,
+				}}
+			></article>
 		</section>
 	);
 }
--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -1,7 +1,26 @@
 import Layout from '../common/Layout';
+import { useRef, useEffect } from 'react';
 
 function Contact() {
-	return <Layout name={'Contact'}></Layout>;
+	//지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조객체생성
+	const container = useRef(null);
+	//일반 HTML버전과는 달리 윈도우객체에서 직접 kakao 상의 객체값을 뽑아옴
+	const { kakao } = window;
+	const option = {
+		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		level: 3, // 지도의 확대 레벨
+	};
+
+	useEffect(() => {
+		//인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
+		new kakao.maps.Map(container.current, option);
+	}, []);
+
+	return (
+		<Layout name={'Contact'}>
+			<div id='map' ref={container}></div>
+		</Layout>
+	);
 }
 
 export default Contact;
