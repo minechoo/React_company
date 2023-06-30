c7df81aa088b99c0f78d3cd7cf78540a705927d8
minechoo <choominhae@gmail.com>
2023-06-30 10:35:51 +0900

    지도 지점

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -4,18 +4,43 @@ import { useRef, useEffect, useState } from 'react';
 function Contact() {
 	const [Traffic, setTraffic] = useState(false);
 	const [Location, setLocation] = useState(null);
+	const [Index, setIndex] = useState(0);
 	//지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조객체생성
 	const container = useRef(null);
+	const arr = useRef(null);
 	//일반 HTML버전과는 달리 윈도우객체에서 직접 kakao 상의 객체값을 뽑아옴
 	const { kakao } = window;
+	const info = [
+		{
+			title: '삼성역 코엑스',
+			latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '넥슨 본사',
+			latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+		{
+			title: '서울 시청',
+			latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+			imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+			imgSize: new kakao.maps.Size(232, 99),
+			imgPos: { offset: new kakao.maps.Point(116, 99) },
+		},
+	];
 	const option = {
-		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
+		center: info[Index].latlng, // 지도의 중심좌표
 		level: 3, // 지도의 확대 레벨
 	};
 	//아래 5개 변수값들은 useEffect 구문에서 인스턴스 생성할때만 필요한 정보값에 불과하므로 미리 읽히도록 useEffect 바깥에 배치
-	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`; // 마커이미지의 주소입니다
-	const imageSize = new kakao.maps.Size(232, 99); // 마커이미지의 크기입니다
-	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const imageSrc = info[Index].imgSrc; // 마커이미지의 주소입니다
+	const imageSize = info[Index].imgSize; // 마커이미지의 크기입니다
+	const imageOption = info[Index].imgPos;
 	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
 
 	const marker = new kakao.maps.Marker({
@@ -30,7 +55,7 @@ function Contact() {
 		marker.setMap(mapInstance);
 		//지역변수의 mapInstance값을 다른 함수에서도 활용해야 되므로 Location state에 해당 인스턴스 값 지정
 		setLocation(mapInstance);
-	}, []);
+	}, [Index]);
 
 	useEffect(() => {
 		//Location state에 담겨있는 앱 인스턴스로부터 traffic레이어 호출구문처리
@@ -44,6 +69,15 @@ function Contact() {
 		<Layout name={'Contact'}>
 			<div id='map' ref={container}></div>
 			<button onClick={() => setTraffic(!Traffic)}>{Traffic ? 'Traffic on' : 'Traffic off'}</button>
+			<ul className='branch'>
+				{info.map((el, idx) => {
+					return (
+						<li key={idx} onClick={() => setIndex(idx)}>
+							{el.title}
+						</li>
+					);
+				})}
+			</ul>
 		</Layout>
 	);
 }
