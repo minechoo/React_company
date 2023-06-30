1d7f49c992c012822340f67bd9410faed9ad2dbf
minechoo <choominhae@gmail.com>
2023-06-30 10:00:47 +0900

    변수 useEffect에서 전역으로

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -12,29 +12,32 @@ function Contact() {
 		center: new kakao.maps.LatLng(33.450701, 126.570667), // 지도의 중심좌표
 		level: 3, // 지도의 확대 레벨
 	};
+	//아래 5개 변수값들은 useEffect 구문에서 인스턴스 생성할때만 필요한 정보값에 불과하므로 미리 읽히도록 useEffect 바깥에 배치
+	const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`; // 마커이미지의 주소입니다
+	const imageSize = new kakao.maps.Size(232, 99); // 마커이미지의 크기입니다
+	const imageOption = { offset: new kakao.maps.Point(116, 99) };
+	const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
+
+	const marker = new kakao.maps.Marker({
+		position: option.center,
+		image: markerImage,
+	});
 
 	useEffect(() => {
 		//인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
 		const mapInstance = new kakao.maps.Map(container.current, option);
 
-		const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`, // 마커이미지의 주소입니다
-			imageSize = new kakao.maps.Size(232, 99), // 마커이미지의 크기입니다
-			imageOption = { offset: new kakao.maps.Point(116, 99) };
-		const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-
-		const marker = new kakao.maps.Marker({
-			position: option.center,
-			image: markerImage,
-		});
-
 		marker.setMap(mapInstance);
+		//지역변수의 mapInstance값을 다른 함수에서도 활용해야 되므로 Location state에 해당 인스턴스 값 지정
 		setLocation(mapInstance);
 	}, []);
 
 	useEffect(() => {
+		//Location state에 담겨있는 앱 인스턴스로부터 traffic레이어 호출구문처리
+		//첫 랜더링 사이클에서는 Location 값이 null이므로 Optionl Changing 을 활용해서 해당값이 담기는 두번째 랜더링 사이클부터 동작하도록 처리
 		Traffic
-			? Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-			: Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+			? Location?.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Location?.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	return (
