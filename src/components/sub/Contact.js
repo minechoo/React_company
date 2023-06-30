9b07c3802fd3e769859397617c5ebe3238ea94d9
minechoo <choominhae@gmail.com>
2023-06-30 09:47:03 +0900

    처음 마운트되는 useErrect와 state변경될때 실행할 코드 분리

--- src/components/sub/Contact.js
+++ src/components/sub/Contact.js
@@ -3,6 +3,7 @@ import { useRef, useEffect, useState } from 'react';
 
 function Contact() {
 	const [Traffic, setTraffic] = useState(false);
+	const [Location, setLocation] = useState(null);
 	//지도가 들어갈 프레임도 가상요소 참조를 위해 useRef로 참조객체생성
 	const container = useRef(null);
 	//일반 HTML버전과는 달리 윈도우객체에서 직접 kakao 상의 객체값을 뽑아옴
@@ -13,21 +14,27 @@ function Contact() {
 	};
 
 	useEffect(() => {
+		//인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
+		const mapInstance = new kakao.maps.Map(container.current, option);
+
 		const imageSrc = `${process.env.PUBLIC_URL}/img/marker1.png`, // 마커이미지의 주소입니다
 			imageSize = new kakao.maps.Size(232, 99), // 마커이미지의 크기입니다
 			imageOption = { offset: new kakao.maps.Point(116, 99) };
 		const markerImage = new kakao.maps.MarkerImage(imageSrc, imageSize, imageOption);
-		//인스턴스 호출구문은 컴포넌트 처음 마운트시 호출
-		const mapInstance = new kakao.maps.Map(container.current, option);
+
 		const marker = new kakao.maps.Marker({
 			position: option.center,
 			image: markerImage,
 		});
 
 		marker.setMap(mapInstance);
+		setLocation(mapInstance);
+	}, []);
+
+	useEffect(() => {
 		Traffic
-			? mapInstance.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
-			: mapInstance.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
+			? Location.addOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC)
+			: Location.removeOverlayMapTypeId(kakao.maps.MapTypeId.TRAFFIC);
 	}, [Traffic]);
 
 	return (
