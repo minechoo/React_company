7a543c1d1d2a9af4236aa75eeb5b5ca192b97e50
minechoo <choominhae@gmail.com>
2023-07-04 12:53:12 +0900

    글 수정기능 추가

--- src/components/sub/Community.js
+++ src/components/sub/Community.js
@@ -4,6 +4,8 @@ import { useRef, useState, useEffect } from 'react';
 function Community() {
 	const input = useRef(null);
 	const textarea = useRef(null);
+	const editInput = useRef(null);
+	const editTextarea = useRef(null);
 	const [Posts, setPosts] = useState([]);
 	const [Allowed, setAllowed] = useState(true);
 
@@ -15,14 +17,14 @@ function Community() {
 	const createPost = () => {
 		if (!input.current.value.trim() || !textarea.current.value.trim()) {
 			resetForm();
-			return alert('내용을 입력하세요');
+			return alert('제목과 본문을 모두 입력하세요.');
 		}
 		setPosts([{ title: input.current.value, content: textarea.current.value }, ...Posts]);
 		resetForm();
 	};
 
 	const deletePost = (delIndex) => {
-		if (!window.confirm('해당 게시글을 삭제하시겠습니까?')) return;
+		if (!window.confirm('해당 게시글을 삭제하겠습니까?')) return;
 		setPosts(Posts.filter((_, idx) => idx !== delIndex));
 	};
 
@@ -39,7 +41,7 @@ function Community() {
 		);
 	};
 
-	const disenableUpdate = (editIndex) => {
+	const disableUpdate = (editIndex) => {
 		setPosts(
 			Posts.map((post, postIndex) => {
 				if (editIndex === postIndex) post.enableUpdate = false;
@@ -50,6 +52,23 @@ function Community() {
 		setAllowed(true);
 	};
 
+	const updatePost = (editIndex) => {
+		if (!editInput.current.value.trim() || !editTextarea.current.value.trim()) {
+			return alert('수정할 제목과 본문을 모두 입력하세요.');
+		}
+
+		setPosts(
+			Posts.map((post, postIndex) => {
+				if (postIndex === editIndex) {
+					post.title = editInput.current.value;
+					post.content = editTextarea.current.value;
+					post.enableUpdate = false;
+				}
+				return post;
+			})
+		);
+	};
+
 	useEffect(() => {
 		console.log(Posts);
 	}, [Posts]);
@@ -57,38 +76,40 @@ function Community() {
 	return (
 		<Layout name={'Community'}>
 			<div className='inputBox'>
-				<input type='text' placeholder='제목을 입력하세요' ref={input} />
+				<input type='text' placeholder='제목을 입력하세요.' ref={input} />
 				<br />
-				<textarea cols='30' rows='3' placeholder='본문을 입력하세요' ref={textarea}></textarea>
+				<textarea cols='30' rows='3' placeholder='본문을 입력하세요.' ref={textarea}></textarea>
 				<br />
+
 				<nav className='btnSet'>
 					<button onClick={resetForm}>cancel</button>
 					<button onClick={createPost}>write</button>
 				</nav>
 			</div>
+
 			<div className='showBox'>
 				{Posts.map((post, idx) => {
 					return (
 						<article key={idx}>
 							{post.enableUpdate ? (
+								//수정모드
 								<>
-									{/* 수정모드 출력 */}
 									<div className='txt'>
-										{/* 인풋요소에 onChange 이벤트로 제어하지 않는 input 요소의 value값은  defaultValue 속성으로 지정*/}
-										{/* value: 리액트의 상태값에 관리되는 폼요소 defaultValue : 일반 돔에 의해 관리되는 폼요소 */}
-										<input type='text' defaultValue={post.title} />
+										{/* onChange이벤트로 제어하지 않는 input요소의 value값은 defaultValue속성으로 지정 */}
+										{/* value: 리액트의 상태값에 관리되는 폼요소, defaultValue: 일반 돔에의해 관리되는 폼요소 */}
+										<input type='text' defaultValue={post.title} ref={editInput} />
 										<br />
-										<textarea defaultValue={post.content} cols='3' rows='10'></textarea>
+										<textarea cols='30' rows='3' defaultValue={post.content} ref={editTextarea}></textarea>
 									</div>
 
 									<nav className='btnSet'>
-										<button onClick={() => disenableUpdate(idx)}>CANCEL</button>
-										<button>UPDATE</button>
+										<button onClick={() => disableUpdate(idx)}>CANCEL</button>
+										<button onClick={() => updatePost(idx)}>UPDATE</button>
 									</nav>
 								</>
 							) : (
+								//출력모드
 								<>
-									{/* 출력모드 */}
 									<div className='txt'>
 										<h2>{post.title}</h2>
 										<p>{post.content}</p>
@@ -111,17 +132,16 @@ function Community() {
 export default Community;
 
 /*
-Create - 데이터 저장(게시글 저장)
-Read - 데이터 호출 (기시글 보기)
-Update - 데이터 수정( 게시글 수정)
-Delete - 데이터 삭제(게시글 삭제)
+Create - 데이터저장 (게시글 저장)
+Read - 데이터호출 (게시글 보기)
+Upated - 데이터수정 (게시글 수정)
+Delete - 데이터삭제 (게시글 삭제)
 
-Restful api 
-localStorage : 모든 브라우저마다 가지고 있는 경량의 데이터 베이스(문자열 저장)
+localStorage: 모든 브라우저마다 가지고 있는 경량의 데이터 베이스 (문자열 저장)
 
 수정 모드 작업 흐름
 1- 수정 버튼 클릭시 해당 순번의 Posts의 객체에 수정관련 property 추가
 2- map으로 반복처리시 수정관련 property의 유무에 따라 수정모드, 출력모드 구분해서 분기처리 후 렌더링
 3- 출력모드: h2, p로 출력 / 수정모드: input, textarea로 값을 담아서 출력 (수정취소, 수정 버튼 추가)
-4- 수정모드에서 수정버튼 클릭시 State값 변경하고 해당 포스트의 수정관련 property 수정 
+4- 수정모드에서 수정버튼 클릭시 State값 변경하고 해당 포스트의 수정관련 property 수정
 */
