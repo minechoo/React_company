4833b3738850ced79cdb31c78150aa24b2023baf
minechoo <choominhae@gmail.com>
2023-07-07 12:34:09 +0900

    menu motion

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,52 +1,64 @@
 import { faBars } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Link, NavLink } from 'react-router-dom';
+import Menu from './Menu';
+import { useRef } from 'react';
 
 function Header({ type }) {
 	// const active = { color: 'aqua' };
 	const active = 'on';
+	const toggleMenu = useRef(null);
 	return (
 		//prop으로 전달되는 type값을 header의 class명으로 지칭해서 스타일 분기처리
-		<header className={type}>
-			<h1>
-				<Link to='/'>LOGO</Link>
-			</h1>
+		<>
+			<header className={type}>
+				<h1>
+					<Link to='/'>LOGO</Link>
+				</h1>
 
-			<ul id='gnb'>
-				<li>
-					<NavLink to='/department' activeClassName={active}>
-						Department
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/community' activeClassName={active}>
-						Community
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/gallery' activeClassName={active}>
-						Gallery
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/youtube' activeClassName={active}>
-						Youtube
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/contact' activeClassName={active}>
-						contact
-					</NavLink>
-				</li>
-				<li>
-					<NavLink to='/member' activeClassName={active}>
-						Member
-					</NavLink>
-				</li>
-			</ul>
+				<ul id='gnb'>
+					<li>
+						<NavLink to='/department' activeClassName={active}>
+							Department
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/community' activeClassName={active}>
+							Community
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/gallery' activeClassName={active}>
+							Gallery
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/youtube' activeClassName={active}>
+							Youtube
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/contact' activeClassName={active}>
+							contact
+						</NavLink>
+					</li>
+					<li>
+						<NavLink to='/member' activeClassName={active}>
+							Member
+						</NavLink>
+					</li>
+				</ul>
 
-			<FontAwesomeIcon icon={faBars} />
-		</header>
+				<FontAwesomeIcon
+					icon={faBars}
+					onClick={() => {
+						toggleMenu.current.toggle();
+					}}
+				/>
+			</header>
+
+			<Menu ref={toggleMenu} />
+		</>
 	);
 }
 
---
+++ src/components/common/Menu.js
@@ -0,0 +1,25 @@
+import { forwardRef, useState, useImperativeHandle } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const Menu = forwardRef((props, ref) => {
+	const [Open, setOpen] = useState(false);
+	useImperativeHandle(ref, () => {
+		return { toggle: () => setOpen(!Open) };
+	});
+	return (
+		<AnimatePresence>
+			{Open && (
+				<motion.nav
+					id='mobilePanel'
+					initial={{ opacity: 0, x: '-100%' }}
+					animate={{ opacity: 1, x: '0%', transition: { duration: 0.5 } }}
+					exit={{ opacity: 0, x: '-100%', transition: { duration: 0.5 } }}
+				>
+					<h1>LOGO</h1>
+				</motion.nav>
+			)}
+		</AnimatePresence>
+	);
+});
+
+export default Menu;
