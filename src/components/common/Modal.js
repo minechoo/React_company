a4c7122c7d5e86b0c131056d1063a425e1991f64
minechoo <choominhae@gmail.com>
2023-07-07 11:01:59 +0900

    motion 사용

--- src/components/common/Modal.js
+++ src/components/common/Modal.js
@@ -1,4 +1,5 @@
 import { forwardRef, useState, useImperativeHandle, useEffect } from 'react';
+import { motion, AnimatePresence } from 'framer-motion';
 
 const Modal = forwardRef((props, ref) => {
 	const [Open, setOpen] = useState(false);
@@ -12,16 +13,25 @@ const Modal = forwardRef((props, ref) => {
 	}, [Open]);
 
 	return (
-		<>
+		//컴포넌트 언마운트시 모션효과가 끝날때까지 언마운트를 자동 지연시켜줌
+		<AnimatePresence>
 			{Open && (
-				<aside className='modal' ref={ref}>
+				//모션은 걸고 싶은 컴포넌트에 motion 지정, initial(모션시작), animate(모션완료), exit(사라지는 모션) 속성 지정
+				//x(가로축), y(세로축), rotate(회전), scale(확대축소)
+				<motion.aside
+					className='modal'
+					ref={ref}
+					initial={{ opacity: 0 }}
+					animate={{ opacity: 1, transition: { duration: 0.5 } }}
+					exit={{ opacity: 0, transition: { duration: 4 } }}
+				>
 					<div className='con'>{props.children}</div>
 					<span className='close' onClick={() => setOpen(false)}>
 						close
 					</span>
-				</aside>
+				</motion.aside>
 			)}
-		</>
+		</AnimatePresence>
 	);
 });
 
