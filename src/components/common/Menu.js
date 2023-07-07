1a85126ba1a1f2f54a402c2541604cb64b8b157c
minechoo <choominhae@gmail.com>
2023-07-07 12:51:55 +0900

    menu 링크까지

--- src/components/common/Menu.js
+++ src/components/common/Menu.js
@@ -1,7 +1,9 @@
 import { forwardRef, useState, useImperativeHandle } from 'react';
 import { motion, AnimatePresence } from 'framer-motion';
+import { Link, NavLink } from 'react-router-dom';
 
 const Menu = forwardRef((props, ref) => {
+	const active = { color: 'aqua' };
 	const [Open, setOpen] = useState(false);
 	useImperativeHandle(ref, () => {
 		return { toggle: () => setOpen(!Open) };
@@ -14,8 +16,43 @@ const Menu = forwardRef((props, ref) => {
 					initial={{ opacity: 0, x: '-100%' }}
 					animate={{ opacity: 1, x: '0%', transition: { duration: 0.5 } }}
 					exit={{ opacity: 0, x: '-100%', transition: { duration: 0.5 } }}
+					onClick={() => setOpen(false)}
 				>
-					<h1>LOGO</h1>
+					<h1>
+						<Link to='/'>LOGO</Link>
+					</h1>
+					<ul id='gnbMo'>
+						<li>
+							<NavLink to='/department' activeStyle={active}>
+								Department
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/community' activeStyle={active}>
+								Community
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/gallery' activeStyle={active}>
+								Gallery
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/youtube' activeStyle={active}>
+								Youtube
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/contact' activeStyle={active}>
+								contact
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to='/member' activeStyle={active}>
+								Member
+							</NavLink>
+						</li>
+					</ul>
 				</motion.nav>
 			)}
 		</AnimatePresence>
