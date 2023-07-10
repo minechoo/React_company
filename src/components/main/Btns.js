cdd5386114d8bf251c1329e1a4dba9fb122da401
minechoo <choominhae@gmail.com>
2023-07-10 12:41:30 +0900

    btns.js: useCallback

--- src/components/main/Btns.js
+++ src/components/main/Btns.js
@@ -1,4 +1,4 @@
-import { useRef, useEffect, useState } from 'react';
+import { useRef, useEffect, useState, useCallback } from 'react';
 import Anime from '../../asset/anime';
 
 function Btns({ setScrolled, setPos }) {
@@ -6,15 +6,15 @@ function Btns({ setScrolled, setPos }) {
 	const pos = useRef([]);
 	const [Num, setNum] = useState(0);
 
-	const getPos = () => {
+	const getPos = useCallback(() => {
 		pos.current = [];
 		const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
 		for (const sec of secs) pos.current.push(sec.offsetTop);
 		setNum(pos.current.length);
 		setPos(pos.current);
-	};
+	}, [setPos]);
 
-	const activation = () => {
+	const activation = useCallback(() => {
 		const base = -window.innerHeight / 2;
 		const scroll = window.scrollY;
 		const btns = btnRef.current.children;
@@ -29,7 +29,7 @@ function Btns({ setScrolled, setPos }) {
 				boxs[idx].classList.add('on');
 			}
 		});
-	};
+	}, [setScrolled]);
 
 	useEffect(() => {
 		getPos();
@@ -43,7 +43,7 @@ function Btns({ setScrolled, setPos }) {
 			window.removeEventListener('scroll', activation);
 			window.scrollTo({ top: 0, left: 0, behavior: 'smooth' });
 		};
-	}, []);
+	}, [getPos, activation]);
 
 	/*
 		eslint가 의존성 배열에 activation, getPos함수를 등록하라고 권고문구를 띄우는 이유
