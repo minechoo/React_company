34c7f533f85245463c9d0844f6f579fd9266245b
minechoo <choominhae@gmail.com>
2023-07-28 09:51:10 +0900

    useQuery 커스텀화

--- src/App.js
+++ src/App.js
@@ -21,19 +21,11 @@ import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { fetchDepartment } from './redux/departmentSlice';
 import { fetchFlickr } from './redux/flickrSlice';
-
-//Menu 컴포넌트를 App새거 호출한 뒤 토글 객체를 각각 메이느 섭브 헤더로 전달해서 토글메뉴기능이 가능하도록
-
-/*
-	redux-toolkit의 작업흐름
-	1.redux 폴더안쪽에 작업하려는 data의 slice 파일 생성 (data fetching후 액션객체 생성함수, 액션객체받아서 전역데이터 수정함수)
-	2.index.js에서 slice값으로 연동된 데이터 store에 저장하고 App에 전달
-	3.app.js에서 slice파일로부터 action객체 생성함수를 import후 호출하여 action만들고 dispatch로 전달
-	4.원하는 컴포넌트에서 useSelector로 데이터 가져오기
-*/
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
 function App() {
-	// const menu = useRef(null);
+	const queryClient = new QueryClient();
 	const dispatch = useDispatch();
 
 	useEffect(() => {
@@ -43,7 +35,7 @@ function App() {
 	}, [dispatch]);
 
 	return (
-		<>
+		<QueryClientProvider client={queryClient}>
 			<Switch>
 				<Route exact path='/' render={() => <Main />} />
 				<Route path='/' render={() => <Header type={'sub'} />} />
@@ -64,7 +56,8 @@ function App() {
 			<Footer />
 
 			<Menu />
-		</>
+			<ReactQueryDevtools />
+		</QueryClientProvider>
 	);
 }
 
