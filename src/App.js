31da70edfc7d8bf7e82f2cc1bf26a6ff0fa083c3
minechoo <choominhae@gmail.com>
2023-08-02 10:06:05 +0900

    18과 비교

--- src/App.js
+++ src/App.js
@@ -19,11 +19,33 @@ import './scss/style.scss';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 
+import { useState } from 'react';
+
 function App() {
+	console.log('render!');
+	const [Count, setCount] = useState(0);
+	const [Count2, setCount2] = useState(0);
 	const queryClient = new QueryClient();
+
+	const returnPromise = () => {
+		return new Promise((res) => setTimeout(res, 500));
+	};
+
+	const handleClick = () => {
+		returnPromise().then(() => {
+			setCount(Count + 1);
+			setCount2(Count2 + 1);
+		});
+	};
 
 	return (
 		<QueryClientProvider client={queryClient}>
+			<div style={{ position: 'fixed', zIndex: 10 }}>
+				<button onClick={handleClick}>button</button>
+				<h1 style={{ color: '#fff' }}>
+					{Count} - {Count2}
+				</h1>
+			</div>
 			<Switch>
 				<Route exact path='/' render={() => <Main />} />
 				<Route path='/' render={() => <Header type={'sub'} />} />
@@ -50,3 +72,9 @@ function App() {
 }
 
 export default App;
+
+/*
+Automatic Batching
+:여러개의 state 값이 하나의 핸들러함수 안쪽에서 동시에 변경될때 그룹으로 묶어서 한번만 랜더링 처리
+:17에도 동작되는 기능이긴 하나 promise 를 반환하는 함수 안쪽에 여러개의 state값이 변경될 경우에는 동작 안 됨
+*/
